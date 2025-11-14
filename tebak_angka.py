450ff95eeed9c8b4c0552ed5e67b56ffbd1a516b
catlord203-rgb <catlord203@gmail.com>
2025-11-14 14:31:57 +0700

    initialcommit

---
+++ tebak_angka.py
@@ -0,0 +1,12 @@
+import random
+class Number_Guessing_Game:
+     def __init__(self,min:int,max:int):
+          self.max=max
+          self.min=min
+          self.max_attempts=5
+          self.reset_game()
+     def reset_game(self):
+        
+
+          
+    
